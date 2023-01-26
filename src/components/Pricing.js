b48518933add420fe75b7b7d35d09ee128ce7e85
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-01-26 10:54:07 -0300

    Pricing and planlist

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { pricing } from '../data'
+import PlanLIst from './PlanList'
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  const { icon, title, plans } = pricing
+  return (
+    <section>
+
+      <div className='section-title-group max-w-[540px] mx-auto px-4 lg:px-6'>
+        <img src={icon} alt="ícone" />
+        <h2 className='h2'>
+          {title} <span className='text-primary-200'>.</span>
+        </h2>
+      </div>
+
+      <PlanLIst plans={plans}/>
+
+
+    </section>
+  )
 };
 
 export default Pricing;
